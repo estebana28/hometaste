3982be0b60a7b991d814e3de78758fbff674d81d
Esteban Arce <eesteban.arce@gmail.com>
2024-01-29 12:27:03 -0300

    swr and chakra installation

---
+++ src/app/fonts.ts
@@ -0,0 +1,18 @@
+// app/fonts.ts
+import { Rubik } from 'next/font/google'
+import { Inter } from "next/font/google";
+
+const rubik = Rubik({
+  subsets: ['latin'],
+  variable: '--font-rubik',
+})
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export const fonts = {
+  rubik,
+  inter
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from './providers'
+import { fonts } from './fonts'
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={fonts.inter.variable}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
